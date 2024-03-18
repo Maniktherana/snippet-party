d31c7d38877941d2a04165b510a1293be22926b7
Manik Rana <manikrana54@gmail.com>
2024-03-18 21:03:16 +0530

    feat: initial commit

---
+++ api/src/db/setup.ts
@@ -0,0 +1,12 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.MYSQL_URL) {
+  throw new Error("DB credentials error");
+}
+const connection = mysql.createConnection(process.env.MYSQL_URL!);
+
+export const db = drizzle(connection);
