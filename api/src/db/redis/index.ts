dd0b64ff812e50f885b296b61a320f29044e3c5b
Manik Rana <manikrana54@gmail.com>
2024-03-19 11:35:36 +0530

    fix(api): use createClient directly

--- api/src/db/redis/index.ts
+++ api/src/db/redis/index.ts
@@ -1,9 +1,9 @@
-import Redis from "redis";
+import { createClient } from "redis";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-export const client = Redis.createClient({
+export const client = createClient({
   url: process.env.REDIS_URL,
 });
 
